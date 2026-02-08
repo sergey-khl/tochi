fixed the quats again. forward pass now works to find phi but idfk what to do for jacobians yet

src/project_3d.py
@@ -43,6 +43,13 @@ def transform_and_project_kernel(
         # Inner loop for projections
         for p_idx in range(num_projections):
             dist = project_on_axis(v_world, projections[p_idx])
+            # wp.printf("body %.2f %.2f %.2f\n", v_body[0], v_body[1], v_body[2])
+            # wp.printf("point %.2f %.2f %.2f\n", p[0], p[1], p[2])
+            # wp.printf("quat %.2f %.2f %.2f %.2f\n", q[0], q[1], q[2], q[3])
+            # wp.printf("world %.2f %.2f %.2f\n", v_world[0], v_world[1], v_world[2])
+            # wp.printf("proj %.2f %.2f %.2f\n", projections[p_idx][0], projections[p_idx][1], projections[p_idx][2])
+            # wp.printf("dist %.2f\n", dist)
+            # wp.printf("------------------------\n\n")
             
             # Calculate flat index for output
             out_idx = v_idx * num_projections + p_idx 
@@ -72,6 +79,9 @@ class WarpWrapper(torch.autograd.Function):
         out_torch = torch.zeros((ctx.batch_n, num_contacts), device=configurations.device)
         ctx.phi_out = wp.from_torch(out_torch)
 
+        print(points)
+        print(projections)
+
         # allocate input
         ctx.configurations = wp.from_torch(configurations)
         ctx.points_wp = wp.from_torch(points, dtype=wp.vec3, requires_grad=True)
@@ -89,6 +99,7 @@ class WarpWrapper(torch.autograd.Function):
     def backward(ctx, phi_out):
         ctx.phi_out.grad = wp.from_torch(phi_out)
 
+
         wp.launch(
             kernel=transform_and_project_kernel,
             dim=ctx.batch_n,
@@ -99,6 +110,12 @@ class WarpWrapper(torch.autograd.Function):
             adjoint=True,
         )
 
+        points = wp.to_torch(ctx.points_wp.grad)
+        projections = wp.to_torch(ctx.projections_wp.grad)
+
+        print(points)
+        print(projections)
+
         # return adjoint w.r.t. inputs
         # TODO: wrong
-        return (wp.to_torch(ctx.points_wp.grad), wp.to_torch(ctx.projections_wp.grad), None)
+        return (points, projections, None)

src/real_cube_dataset.py
@@ -47,11 +47,6 @@ class RealCubeDataset(Dataset):
 
         # in contactnets they use quat convention of (w, x, y, z)
         qw, qx, qy, qz = new_state[:, 3].clone(), new_state[:, 4].clone(), new_state[:, 5].clone(), new_state[:, 6].clone()
-        # same as before, flip y and z and invert y
-        q_converted_x = qx
-        q_converted_y = -qz
-        q_converted_z = qy
-        q_converted_w = qw
 
         rad = np.radians(-90) # find using right hand rule (curl da fingies)
         corr_x = np.sin(rad / 2)
@@ -59,10 +54,10 @@ class RealCubeDataset(Dataset):
         corr_z = 0.0
         corr_w = np.cos(rad / 2)
         # quaternion rotation magic formula
-        new_state[:, 3] = q_converted_w * corr_x + q_converted_x * corr_w + q_converted_y * corr_z - q_converted_z * corr_y
-        new_state[:, 4] = q_converted_w * corr_y - q_converted_x * corr_z + q_converted_y * corr_w + q_converted_z * corr_x
-        new_state[:, 5] = q_converted_w * corr_z + q_converted_x * corr_y - q_converted_y * corr_x + q_converted_z * corr_w
-        new_state[:, 6] = q_converted_w * corr_w - q_converted_x * corr_x - q_converted_y * corr_y - q_converted_z * corr_z
+        new_state[:, 3] = qw * corr_x + qx * corr_w + qy * corr_z - qz * corr_y
+        new_state[:, 4] = qw * corr_y - qx * corr_z + qy * corr_w + qz * corr_x
+        new_state[:, 5] = qw * corr_z + qx * corr_y - qy * corr_x + qz * corr_w
+        new_state[:, 6] = qw * corr_w - qx * corr_x - qy * corr_y - qz * corr_z
 
         return new_state
 

src/trainer.py
@@ -35,7 +35,10 @@ class Trainer:
 
                 self.optimizer.zero_grad()
                 out_grads = self.contact_net.phi_net.forward(prev_config)
-                out_grads.backward(torch.ones_like(out_grads))
+                print(out_grads)
+                out = out_grads.backward(torch.ones_like(out_grads))
+                print(out)
+                assert False
 
                 self.optimizer.step()
 